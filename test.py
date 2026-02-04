c83dc8c0695d90f84e90d44b560d0ea6abd259a2
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-04 09:31:53 +0100

    Persönliche abfragen mit Python

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 print("Hello, World!")
 print("Habs herausgefunden!")
+
+# Persönliche Informationen abfragen
+namen = input("Gib deinen Namen ein: ")
+print("Hallo, " + namen )
+alter = input("Wie alt bist du? ")
+print("Du bist also" + alter + " Jahre alt.")
+gewicht = input("Wie viel wiegst du? ")
+print("Du wiegst " + gewicht + " kg.")
+größe = input("Wie groß bist du (in cm)? ")
+print("Du bist " + größe + " cm groß.")
+hobby = input("Was ist dein Hobby? ")
+print("Dein Hobby ist " + hobby + ".")
+lieblingsfarbe = input("Was ist deine Lieblingsfarbe? ")
+print("Deine Lieblingsfarbe ist " + lieblingsfarbe + ".")
