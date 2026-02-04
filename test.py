5e40e3d49487122fcaee4294d36c2dd35a3306cc
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-04 09:34:38 +0100

    Abfragen mit if Else

--- test.py
+++ test.py
@@ -14,3 +14,18 @@ hobby = input("Was ist dein Hobby? ")
 print("Dein Hobby ist " + hobby + ".")
 lieblingsfarbe = input("Was ist deine Lieblingsfarbe? ")
 print("Deine Lieblingsfarbe ist " + lieblingsfarbe + ".")
+
+# Abfragen mit If Else
+note = float(input("Welche Note hast du in deinem letzten Test bekommen? "))
+if note >= 1.0 and note <= 1.5:
+    print("Sehr gut!")
+elif note >= 2.0 and note <= 2.5:
+    print("Gut!")
+elif note >= 3.0 and note <= 3.5:
+    print("Befriedigend!")
+elif note >= 4.0 and note <= 4.5:
+    print("Ausreichend!")
+else:
+    print("Nicht ausreichend!")
+
+
