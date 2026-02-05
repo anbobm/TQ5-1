20e09745ee5c6192905bf1a0597b5aa4c5ec2fd3
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-05 12:19:57 +0100

    test.py änderung

--- test.py
+++ test.py
@@ -8,3 +8,19 @@ for i in range(number):
         print("0")
     else:
         print(ausgangszahl)
+
+
+number = input()
+number = int(number)
+ausgabe = 0
+
+for i in range(2, number, 2):
+    print(i)
+    print("+")
+    if i > ausgabe:
+        ausgabe = ausgabe + i
+else:
+    print(number)
+    print("=")
+    print(ausgabe+number)
+    
