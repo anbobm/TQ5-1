7a0f3fe874b07b981f0d866278f755bf8715bbc3
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-06 11:34:43 +0100

    test.py funktion kam dazu

--- test.py
+++ test.py
@@ -1,26 +1,33 @@
 
 
-number = int(input("Gib eine Zahl ein: "))
-for i in range(number):
-    ausgangszahl = number - i
-    if ausgangszahl  == 1:
-        print("1")
-        print("0")
-    else:
-        print(ausgangszahl)
+# number = int(input("Gib eine Zahl ein: "))
+# for i in range(number):
+#     ausgangszahl = number - i
+#     if ausgangszahl  == 1:
+#         print("1")
+#         print("0")
+#     else:
+#         print(ausgangszahl)
 
 
-number = input()
-number = int(number)
-ausgabe = 0
+# number = input()
+# number = int(number)
+# ausgabe = 0
 
-for i in range(2, number, 2):
-    print(i)
-    print("+")
-    if i > ausgabe:
-        ausgabe = ausgabe + i
-else:
-    print(number)
-    print("=")
-    print(ausgabe+number)
+# for i in range(2, number, 2):
+#     print(i)
+#     print("+")
+#     if i > ausgabe:
+#         ausgabe = ausgabe + i
+# else:
+#     print(number)
+#     print("=")
+#     print(ausgabe+number)
     
+
+
+def add_3(zahl):
+    zahl = zahl + 3
+    print(zahl)
+
+add_3("5")
