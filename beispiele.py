9ec61cc6a8d4d9b2da55a268b4fda5cd598dee68
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-09 17:50:41 +0100

    Add Aufgabe 1 und Aufgabe 2 (Zahl prüfen)

---
+++ beispiele.py
@@ -0,0 +1,80 @@
+# eingabe = input()
+# zahl = int(eingabe)
+# zahl = zahl + 1
+# print(zahl)
+
+# name = input("Wie heißt du? ")
+# print("Dein Name ist " + name)
+
+# alter = int(input("Gib dein Alter ein: "))
+# alter = alter + 10
+# print("In 10 Jahren bist du " + str(alter) + " Jahre alt")
+
+# zahl = 34
+# zahl2 = 35
+# summe = zahl + zahl2
+# print(summe)
+
+# dividend = 7
+# divisor = 3
+# ganzzahliger_quotient = 7 // 3
+# rest = 7 % 3
+# print(str(dividend) + " durch " + str(divisor) + " ist " + str(ganzzahliger_quotient) + " Rest " + str(rest))
+
+# compound assignments
+# zahl = 5
+
+# zahl += 3
+# zahl = zahl + 3
+
+# zahl -= 3
+# zahl = zahl - 3
+
+# zahl *= 3
+# zahl = zahl * 3
+
+# zahl /= 3
+# zahl = zahl / 3
+
+# zahl //= 3
+# zahl = zahl // 3
+
+# zahl %= 3
+# zahl = zahl % 3
+
+# def zieh_ab(zahl, noch_eine_zahl):
+#     result = zahl - noch_eine_zahl
+#     return result
+
+# variable = 11
+# variable2 = 15
+# rückgabewert = zieh_ab(variable, variable2)
+# print(rückgabewert)
+
+def add_3(zahl):
+    print()
+    print()
+    return zahl + 3
+variable = 11
+rückgabewert = add_3(variable)
+print(rückgabewert)
+
+
+#========Eigene Beispiele=========
+#eingabe = input()
+#zahl = int(eingabe)
+#zahl = zahl + 1
+#print(zahl)
+zahl = 10
+zahl = zahl + 1
+print(zahl)
+
+zahl = 10
+print(zahl)
+zahl = zahl + 3
+print(zahl)
+
+zahl = 10
+print(zahl)
+zahl = zahl - 2
+print(zahl)
