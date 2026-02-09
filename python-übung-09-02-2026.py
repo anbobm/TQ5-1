48ebfb83d49f24fb970a974fffed4b7ecf17aae0
ihornosach <257969028+ihornosach@users.noreply.github.com>
2026-02-09 11:24:55 +0100

    Add Übung 09.02.2026

---
+++ python-übung-09-02-2026.py
@@ -0,0 +1,33 @@
+#Exercise 1
+
+zahl = 10
+
+if zahl >= 10:
+    print("Zahl ist größer oder gleich 10")
+else:
+    print("Zahl ist kleiner als 10")
+
+#Exercise 2
+
+zahl = input("Geben Sie eine Zahl ein: ")
+
+if zahl >= 10:
+    print("Zahl ist größer oder gleich 10")
+else:    
+    print("Zahl ist kleiner als 10")
+
+#Exercise 3
+
+alter = input("Geben Sie Ihr Alter ein: ")
+
+if alter < 18:
+    print("Sie sind minderjährig.")
+else:
+    print("Sie sind volljährig.")
+
+#Exercise 4
+
+password = input("Geben Sie Ihr Passwort ein: ")
+
+if password == "password123":
+    print("Du hast das Passwort erraten!")
