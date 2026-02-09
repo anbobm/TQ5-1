c80d1392605e6525d06a88e8986959c2d3fc36df
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-09 11:31:45 +0100

    Aufgabe von 09.02.2026

---
+++ Aufgabe 2026-02-09.py
@@ -0,0 +1,29 @@
+# Aufgabe 1
+zahl = 8
+
+if zahl >= 10:
+    print("Die Zahl ist größer oder gleich 10.")
+else:
+    print("Die Zahl ist kleiner als 10.")
+
+# Aufgabe 2
+zahl = input("Gib eine Zahl ein: ")
+
+if zahl >= 10:
+    print("Die Zahl ist größer oder gleich 10.")
+else:
+    print("Die Zahl ist kleiner als 10.")
+
+# Aufgabe 3
+alter = int(input("Gib dein Alter ein: "))
+
+if alter >= 18:
+    print("Du bist volljährig.")
+else:
+    print("Du bist minderjährig.")
+
+# Aufgabe 4
+passwort = input("Gib dein Passwort ein: ")
+
+if passwort == "password123":
+    print("Du hast das Passwort erraten!")
