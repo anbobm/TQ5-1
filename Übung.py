7c321e2dd685ee0c7592db0ae1223ff8f9c97d24
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-10 09:43:09 +0100

    Übung mit der Temperatur

--- Übung.py
+++ Übung.py
@@ -1,17 +1,28 @@
-zahl = int(input("Gib eine Zahl ein: "))
+# zahl = int(input("Gib eine Zahl ein: "))
 # if zahl % 2 == 0:
 #     print("Die Zahl ist gerade.")
 # else:    
 #     print("Die Zahl ist ungerade.")
 
-def isEven(zahl):
-    if zahl % 2 == 0:
-        return True
-    else:
-        return False
+# def isEven(zahl):
+#     if zahl % 2 == 0:
+#         return True
+#     else:
+#         return False
 
-Ausgabe = isEven(zahl)
-if Ausgabe == True:
-    print("Die Zahl ist gerade.")
+# Ausgabe = isEven(zahl)
+# if Ausgabe == True:
+#     print("Die Zahl ist gerade.")
+# else:
+#     print("Die Zahl ist ungerade.")
+
+temperatur = int(input("Gib die Temperatur ein: "))
+if temperatur < 0:
+    print("Es ist eiskalt.")
+elif temperatur < 20:
+    print("Es ist frisch.")
+elif temperatur < 30:
+    print("Es ist warm.")
 else:
-    print("Die Zahl ist ungerade.")
+    print("Es ist heiß.")
+
