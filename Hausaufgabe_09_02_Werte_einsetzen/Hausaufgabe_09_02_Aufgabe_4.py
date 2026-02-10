0805bd6b91e0ae60eb1b64390fca405e422b5783
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-10 08:12:30 +0100

    Add Aufgabe 4 (Passwort prüfen)

---
+++ Hausaufgabe_09_02_Werte_einsetzen/Hausaufgabe_09_02_Aufgabe_4.py
@@ -0,0 +1,9 @@
+# Aufgabe 4
+# Der Benutzer soll ein Passwort eingeben.
+# Wenn der Benutzer "password123" eingibt, soll ausgegeben werden:
+# "Du hast das Passwort erraten!" (ansonsten passiert nichts)
+
+passwort = input("Gib das Passwort ein: ")
+
+if passwort == "password123":
+    print("Du hast das Passwort erraten!")
