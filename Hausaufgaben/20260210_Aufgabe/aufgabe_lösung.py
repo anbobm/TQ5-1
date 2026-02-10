dd8fd177430c9566a803752c53db0aed912f1f63
ihornosach <257969028+ihornosach@users.noreply.github.com>
2026-02-10 11:54:47 +0100

    vriable changes

--- Hausaufgaben/20260210_Aufgabe/aufgabe_lösung.py
+++ Hausaufgaben/20260210_Aufgabe/aufgabe_lösung.py
@@ -3,24 +3,24 @@ import random
 sum = 0
 bonus_punkte = 0
 
-wurf1 = wurf2 = wurf3 = 0
+wurf_1 = wurf_2 = wurf_3 = 0
 
 for i in range(3):
     würfel = random.randint(1, 6)
 
     if i == 0:
-        wurf1 = würfel
+        wurf_1 = würfel
     elif i == 1:
-        wurf2 = würfel
+        wurf_2 = würfel
     else:
-        wurf3 = würfel
+        wurf_3 = würfel
 
     print(f"Wurf {i + 1}: {würfel}")
     sum += würfel
 
-if wurf1 == wurf2 == wurf3:
+if wurf_1 == wurf_2 == wurf_3:
     bonus_punkte += 6
-if wurf1 == wurf2 or wurf1 == wurf3 or wurf2 == wurf3:
+if wurf_1 == wurf_2 or wurf_1 == wurf_3 or wurf_2 == wurf_3:
     bonus_punkte += 2
 
 print(f"Summe der Würfe: {sum} und bonus Punkte: {bonus_punkte}")
