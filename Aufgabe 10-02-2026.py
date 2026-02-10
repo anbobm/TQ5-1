fe1fc78f43b3da46c94aaa8cafaf4ac660ff0d1a
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-10 10:37:16 +0100

    Notenübung

---
+++ Aufgabe 10-02-2026.py
@@ -0,0 +1,14 @@
+
+import random
+
+zufallszahl = random.randint(1, 6)
+zufallszahl2 = random.randint(1, 6)
+zufallszahl3 = random.randint(1, 6)
+PashBonus = 2
+Summe = zufallszahl + zufallszahl2 + zufallszahl3
+
+if zufallszahl == zufallszahl2:
+    print("Du hast ein Pash geworfen!")
+
+elif zufallszahl == zufallszahl3:
+    print("Du hast ein Pash geworfen!")
--- Übung.py
+++ Übung.py
@@ -16,13 +16,27 @@
 # else:
 #     print("Die Zahl ist ungerade.")
 
-temperatur = int(input("Gib die Temperatur ein: "))
-if temperatur < 0:
-    print("Es ist eiskalt.")
-elif temperatur < 20:
-    print("Es ist frisch.")
-elif temperatur < 30:
-    print("Es ist warm.")
-else:
-    print("Es ist heiß.")
+# temperatur = int(input("Gib die Temperatur ein: "))
+# if temperatur < 0:
+#     print("Es ist eiskalt.")
+# elif temperatur < 20:
+#     print("Es ist frisch.")
+# elif temperatur < 30:
+#     print("Es ist warm.")
+# else:
+#     print("Es ist heiß.")
 
+
+benutzer_note= int(input("Gib deine Note ein: "))
+if benutzer_note >= 92:
+    print("Sehr gut")
+elif benutzer_note >= 81:
+    print("Gut")
+elif benutzer_note >= 67:
+    print("Befriedigend")
+elif benutzer_note >= 50:
+    print("Ausreichend")
+elif benutzer_note >= 30:
+    print("Mangelhaft")
+else:
+    print("Ungenügend")
