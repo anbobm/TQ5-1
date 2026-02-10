3e01f0e97fe247e9b16b419442f6243e28c954d8
emmaraupe <257969851+emmaraupe@users.noreply.github.com>
2026-02-10 09:07:53 +0100

    zusatzaufgabe hinzugefuegt

--- 20260210_uebung.py
+++ 20260210_uebung.py
@@ -2,3 +2,17 @@ zahl = int(input("Geben Sie eine gerade Zahl ein: "))
 if zahl % 2 == 0:
     print("Glückwunsch, die Zahl ist gerade.")
 else:    print("Du solltest noch mal zur Schule gehen.")
+
+#Funktion isEven(zahl) schreiben, die true zurückgibt, 
+#wenn die übergebene Zahl gerade ist, ansonsten false
+
+def isEven(zahl):
+    if zahl % 2 == 0:
+        return True
+    else:
+        return False
+    
+inputZahl = int(input("Geben Sie eine Zahl ein: "))
+if isEven(inputZahl):
+    print("gerade.")
+else:    print("ungerade.")
