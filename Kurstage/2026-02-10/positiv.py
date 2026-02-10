5c4471f3d911ba8503bb6934ba3fe72fad576334
Andreas <foo@bar.com>
2026-02-10 09:56:49 +0100

    Aufgabe und Lösung positiv-negativ-0 zugefügt

---
+++ Kurstage/2026-02-10/positiv.py
@@ -0,0 +1,13 @@
+# positive Zahlen: 1, 2, 3 ..
+# negative Zahlen: -1, -2, -3
+# die Null ist weder positiv noch negativ
+
+zahl = int(input("Zahl! "))
+
+if zahl > 0:
+    print("Die Zahl ist positiv")
+elif zahl < 0:
+    print("Die Zahl ist negativ")
+else:
+    print("Die Zahl ist 0")
+
