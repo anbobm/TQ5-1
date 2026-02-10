7929dfb54ff965360ceabc3d3c8b2258c0228a2f
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-10 09:10:00 +0100

    Übung wurde erweitert

--- Übung.py
+++ Übung.py
@@ -1,5 +1,17 @@
 zahl = int(input("Gib eine Zahl ein: "))
-if zahl % 2 == 0:
+# if zahl % 2 == 0:
+#     print("Die Zahl ist gerade.")
+# else:    
+#     print("Die Zahl ist ungerade.")
+
+def isEven(zahl):
+    if zahl % 2 == 0:
+        return True
+    else:
+        return False
+
+Ausgabe = isEven(zahl)
+if Ausgabe == True:
     print("Die Zahl ist gerade.")
-else:    
+else:
     print("Die Zahl ist ungerade.")
