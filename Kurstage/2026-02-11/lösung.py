13bf0052e8b67c0a72baf63bc46df4827e049464
ihornosach <257969028+ihornosach@users.noreply.github.com>
2026-02-11 10:14:36 +0100

    lösung für aufgabe 11.02.2026

---
+++ Kurstage/2026-02-11/lösung.py
@@ -0,0 +1,15 @@
+#for i in range(21):
+#    print(i)
+
+#i = 0
+#while i <= 20:
+#    print(i)
+#    i = i + 1
+
+#for i in range(13,26):
+#    print(i)
+
+i = 13
+while i <= 25:
+    print(i)
+    i = i + 1
