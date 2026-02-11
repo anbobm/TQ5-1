3539dc22337fd4dabdffe1dbb32047b94179a1fd
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-11 16:05:55 +0100

    Aufgabe 2: Zahlen 13 bis 25 (for und while)

--- Kurstage/2026-02-11/for.py
+++ Kurstage/2026-02-11/for.py
@@ -4,3 +4,8 @@
 for zahl in range(21):
     print(zahl)
 
+# Aufgabe 2
+# mit for
+
+for zahl in range(13, 26):
+    print(zahl)
---
+++ Kurstage/2026-02-11/while.py
@@ -0,0 +1,14 @@
+#Aufgabe 1
+#mit while
+zahl = 0
+while zahl <= 20:
+    print(zahl)
+    zahl += 1
+
+  # Aufgabe 2
+# mit while
+
+zahl = 13
+while zahl <= 25:
+    print(zahl)
+    zahl += 1
