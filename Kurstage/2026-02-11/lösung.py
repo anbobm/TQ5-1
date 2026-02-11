a3143f58586f958dea464c1597f31b946b0b28e1
ihornosach <257969028+ihornosach@users.noreply.github.com>
2026-02-11 10:39:18 +0100

    lösung zugefügt

--- Kurstage/2026-02-11/lösung.py
+++ Kurstage/2026-02-11/lösung.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #for i in range(21):
 #    print(i)
 
@@ -13,3 +14,37 @@ i = 13
 while i <= 25:
     print(i)
     i = i + 1
+
+# Aufgabe 1
+# Gib die Zahlen von 0 bis 20 aus: 0, 1, ..., 20
+# Einmal mit Hilfe von `for .. in ..:` und einmal mit Hilfe von  `while`.
+
+# mit for
+zahlen = range(21)
+
+for zahl in zahlen:
+    print(zahl)
+
+# # mit while
+zahl = 0
+
+while zahl <= 20:
+    print(zahl)
+    zahl = zahl + 1
+
+# Aufgabe 2
+# Gib die Zahlen von 13 bis 25 aus: 13, 14, ... , 25
+# Einmal mit Hilfe von `for .. in ..:` und einmal mit Hilfe von  `while`.
+
+# for
+
+for zahl in range(13, 26):
+    print(zahl)
+
+# while
+
+zahl = 13
+
+while zahl <= 25:
+    print(zahl)
+    zahl = zahl + 1
