2f63390b3bf7adc31b9dc5b2095486c2e7d1c87c
Andreas <foo@bar.com>
2026-02-11 11:13:18 +0100

    While Beispiele geupdatet

--- Kurstage/2026-02-11/while.py
+++ Kurstage/2026-02-11/while.py
@@ -1,5 +1,13 @@
+# Schleife die die Zahlen von 0 bis 9 ausgibt
 n = 0
 
 while n < 10:
     print(n)
     n = n + 1
+
+# Schleife die den Benutzer so lange etwas eingeben lässt, bis er "ende" eingibt
+eingabe = ""
+
+while eingabe != "ende":
+    eingabe = input()
+    print(f"Du hast {eingabe} eingegeben")
