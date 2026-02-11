437da0683cd8fb774b6bb292f476f5f859f4bcb8
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-11 14:35:31 +0100

    Aufgaben von Heute

---
+++ Hausaufgabe_11-2-2026.py
@@ -0,0 +1,52 @@
+# Aufgabe 1
+rechnen = int(input("Gebe eine Zahl ein: "))
+
+for i in range(1, 11):
+    print(rechnen, "*", i, "=", rechnen * i)
+
+# Aufgabe 2
+import random
+suchende_zahl = random.randint(1, 10)
+gefunden = False
+
+while gefunden == False:
+    eingabe = int(input("Gebe eine Zahl zwischen 1 und 10 ein: "))
+    if eingabe == suchende_zahl:
+        print("Du hast die Zahl gefunden!")
+        gefunden = True
+    else:
+        print("Leider falsch, versuche es nochmal!")
+
+# Aufgabe 3
+import random
+suchende_zahl = random.randint(1, 10)
+gefunden = False
+
+while gefunden == False:
+    eingabe = int(input("Gebe eine Zahl zwischen 1 und 10 ein: "))
+    if eingabe == suchende_zahl:
+        print("Du hast die Zahl gefunden!")
+        gefunden = True
+    elif eingabe < suchende_zahl:
+        print("Die gesuchte Zahl ist größer als deine Eingabe. Versuche es nochmal!")
+    else:
+        print("Die gesuchte Zahl ist kleiner als deine Eingabe. Versuche es nochmal!")
+
+# Aufgabe 4
+import random
+suchende_zahl = random.randint(1, 10)
+gefunden = False
+
+while gefunden == False:
+    eingabe = int(input("Gebe eine Zahl zwischen 1 und 10 ein: "))
+    if eingabe == suchende_zahl:
+        print("Du hast die Zahl gefunden!")
+        nochmal = input("Möchtest du nochmal spielen? (ja/nein): ")
+        if nochmal == "ja":
+            suchende_zahl = random.randint(1, 10)
+        else:
+            gefunden = True
+    elif eingabe < suchende_zahl:
+        print("Die gesuchte Zahl ist größer als deine Eingabe. Versuche es nochmal!")
+    else:
+        print("Die gesuchte Zahl ist kleiner als deine Eingabe. Versuche es nochmal!")
---
+++ Übungsaufgabe 11-2.py
@@ -0,0 +1,54 @@
+# Aufgabe 1
+zahlen = range(21)
+
+for zahl in zahlen:
+    print(zahl)
+
+
+zahl = 0
+
+while zahl <= 20:
+    print(zahl)
+    zahl = zahl + 1
+
+# Aufgabe 2
+zahlen = range(13,26)
+
+for zahl in zahlen:
+    print(zahl)
+
+while zahl <= 25:
+    print(zahl)
+    zahl = zahl + 1
+
+# Aufgabe 3
+summe = 0
+for zahl in range(101):
+    summe = summe + zahl
+print(summe)
+
+summe = 0
+zahl = 0
+
+while zahl <= 100:
+    summe = summe + zahl
+    zahl = zahl + 1
+print(summe)
+
+# Aufgabe 4
+summe = 0
+zahlen = range(1,101,2)
+
+for zahl in zahlen:
+    summe = summe + zahl
+    
+print(summe)
+
+summe = 0
+zahl = 0
+
+while zahl <= 100:
+    if zahl % 2 == 0:
+        summe = summe + zahl
+    zahl = zahl + 1
+print(summe)
