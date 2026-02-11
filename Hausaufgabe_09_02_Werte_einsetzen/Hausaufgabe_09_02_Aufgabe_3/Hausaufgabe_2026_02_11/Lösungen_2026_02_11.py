a201403aca801b758f7c0b38f7d29ca9c5b3fbb4
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-11 20:18:47 +0100

    Hausaufgabe 11.02 – Aufgabe 3

---
+++ Hausaufgabe_09_02_Werte_einsetzen/Hausaufgabe_09_02_Aufgabe_3/Hausaufgabe_2026_02_11/Lösungen_2026_02_11.py
@@ -0,0 +1,45 @@
+# Aufgabe 1
+# Der Benutzer soll eine Zahl eingeben.
+# Anschließend werden die Vielfachen dieser Zahl ausgegeben.
+# Ausgabe von 1 bis 10
+
+zahl = int(input("Bitte gib eine Zahl ein: "))
+
+for i in range(1, 11):
+    print(f"{i} x {zahl} = {i * zahl}")
+
+
+    # Aufgabe 2
+# Das Programm würfelt eine Zahl zwischen 1 und 10.
+# Der Benutzer muss die Zahl erraten.
+# Das Programm endet bei richtiger Eingabe.
+
+import random
+
+zahl = random.randint(1, 10)
+tipp = 0
+
+while tipp != zahl:
+    tipp = int(input("Rate die Zahl zwischen 1 und 10: "))
+
+print("Richtig geraten!")   
+
+
+
+# Aufgabe 3
+# Der Benutzer soll erraten, ob die Zahl zu hoch oder zu niedrig ist.
+
+import random
+
+zahl = random.randint(1, 10)
+tipp = 0
+
+while tipp != zahl:
+    tipp = int(input("Rate die Zahl zwischen 1 und 10: "))
+
+    if tipp < zahl:
+        print("Zu niedrig")
+    elif tipp > zahl:
+        print("Zu hoch")
+
+print("Richtig geraten!")
