ae26592f7b8b708ecc98cae8cb4ef7595585333a
katjaarens <257968712+katjaarens@users.noreply.github.com>
2026-02-11 10:15:22 +0100

    Aufgaben eingetragen 2026-02-11

---
+++ 2026-02-11 Aufgaben.py
@@ -0,0 +1,36 @@
+# Aufgabe 1
+
+#Gib die Zahlen von 0 bis 20 aus: 0, 1, ..., 20
+#Einmal mit Hilfe von `for .. in ..:` und einmal mit Hilfe von  `while`.
+
+#Lösung mit FOR Schleife
+
+for i in range(21):
+    print(i)
+
+# Lösung mit WHILE Schleife
+
+i = 0
+while i <= 20:
+    print(i)
+    i = i + 1    
+
+
+  # Aufgabe 2
+
+#Gib die Zahlen von 13 bis 25 aus: 13, 14, ... , 25
+#Gib die Zahlen von 13 bis 25 aus: 13, 14, ... , 25
+#Einmal mit Hilfe von `for .. in ..:` und einmal mit Hilfe von  `while`.  
+
+# Lösung mit FOR Schleife 
+
+for i in range(13, 26):
+    print(i)
+
+
+#Lösung mit While Schleife
+
+i = 13
+while i <= 25:
+    print(i)
+    i = i + 1
