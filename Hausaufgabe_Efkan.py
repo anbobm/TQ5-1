0679690044aa2e271e5f3d72a6215a636b5a9df4
Efkan <efkancifteci35@gmail.com>
2026-02-11 13:43:38 +0100

    Abgabe Hausaufgabe

---
+++ Hausaufgabe_Efkan.py
@@ -0,0 +1,31 @@
+#Aufgabe 1
+
+# zahl = int(input("Zahl eingeben:"))
+# for i in range(0, 11):
+#     print(zahl)
+#     ergebnis = i * zahl
+#     print(f"{i} x {zahl} = {ergebnis}")
+
+#Aufgabe 2
+
+# import random
+# w1 = random.randint(1, 11)
+# zahl = 0 
+# while zahl != w1:
+#     zahl = int(input("Rate:"))
+# zahl = w1
+# print("Richtig du hast es Erraten:")
+
+#Aufgabe 3
+# import random
+# w1 = random.randint(1,11)
+# zahl = 0
+# while zahl != w1:
+#     zahl = int(input("Rate:"))
+#     if zahl < w1:
+#         print("zu niedrig.:")
+#     elif zahl > w1:
+#         print("zu hoch.:")
+# print("Klasse du hast es Erraten.:")
+    
+    
