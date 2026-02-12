6cc9e9fe863b48b3d1fa455516348f9781f243f7
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-12 09:48:18 +0100

    Übungsaufgabe 12-2-26

---
+++ Übung 12-2-2026.py
@@ -0,0 +1,18 @@
+# Aufgabe 1
+zahl = 10
+while zahl >= 1:
+    print(zahl)
+    zahl = zahl - 1
+
+# Aufgabe 2
+faktorial = int(input("Gib eine Zahl ein: "))
+multipliziert = 1
+
+for i in range(1, faktorial + 1):
+    multipliziert = multipliziert * i
+    
+
+print(multipliziert)
+
+
+
