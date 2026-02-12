83fae2b24ae5829cb3f3b4fc7bc4641c0dba2501
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-12 10:49:17 +0100

    Erweiterung der Aufgaben

--- Übung 12-2-2026.py
+++ Übung 12-2-2026.py
@@ -14,5 +14,27 @@ for i in range(1, faktorial + 1):
 
 print(multipliziert)
 
+# Aufgabe 4
+satz = input("Gib einen Satz ein: ")
+Vokale = 0
+for buchstabe in satz:
+    if buchstabe == "a" or buchstabe == "e" or buchstabe == "i" or buchstabe == "o" or buchstabe == "u":
+        Vokale = Vokale + 1
+    else:
+        continue
 
+print("Der Satz enthält", Vokale, "Vokale.")
 
+# Aufgabe 5
+
+frage = True
+Zusatz = 0
+while frage == True:
+    eingabe = int(input("Gib eine Zahl ein: "))
+    if eingabe == 0:
+        frage = False
+    else:
+        Zusatz = Zusatz + eingabe
+        
+
+print("Die Summe der eingegebenen Zahlen ist:", Zusatz)
