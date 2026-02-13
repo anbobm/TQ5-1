801138459f28cac11921d6c1d8aafc6811438de2
ihornosach <257969028+ihornosach@users.noreply.github.com>
2026-02-13 11:09:28 +0100

    lösung

--- Hausaufgaben/20260213_Aufgabe/lösung.py
+++ Hausaufgaben/20260213_Aufgabe/lösung.py
@@ -1,5 +1,51 @@
-square = lambda n: n * n
+# #Aufgabe 1 
 
-print(square(4)) #16
-print(square(5)) #25
+zahl_a = int(input("Zahl a? "))
+zahl_b = int(input("Zahl b? "))
+operator = input("Operator?(*,+,-,/): ")
+
+match operator:
+    case "+":
+        print(f"{zahl_a} + {zahl_b} = {zahl_a + zahl_b}")
+    case "-":
+        print(f"{zahl_a} - {zahl_b} = {zahl_a - zahl_b}")
+    case "*":
+        print(f"{zahl_a} * {zahl_b} = {zahl_a * zahl_b}")
+    case "/":
+        print(f"{zahl_a} / {zahl_b} = {zahl_a / zahl_b}")
+
+# #Aufgabe 2 
+
+max_zahl = int(input("Zahl 1? "))
+
+for iter in range(2):
+    zahl = int(input(f"Zahl {iter + 2}? "))
+    if zahl > max_zahl:
+        max_zahl = zahl
+
+print(max_zahl)
+
+#Aufgabe 3
+
+#mit for
+
+zahl = int(input("Geben Sie ein Zahl ein: "))
+summe = 0
+
+for iter in range(1, zahl + 1):
+    if iter % 3 == 0:
+        summe = summe + iter
+
+print(summe)
+
+#mit while
+
+i = 1
+
+while iter <= zahl:
+    if iter % 3 == 0:
+        summe += iter
+    iter = iter + 1
+
+print(summe)
 
