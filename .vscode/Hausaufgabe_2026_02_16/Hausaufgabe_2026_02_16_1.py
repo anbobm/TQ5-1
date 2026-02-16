ce59b388f698f93f9e59020507258e7da16d587e
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-16 14:16:56 +0100

    Aufgabe 2 erledigt

--- .vscode/Hausaufgabe_2026_02_16/Hausaufgabe_2026_02_16_1.py
+++ .vscode/Hausaufgabe_2026_02_16/Hausaufgabe_2026_02_16_1.py
@@ -11,3 +11,18 @@ def größtes_element(liste):
 
 # Test / Kontrolle
 print(größtes_element([3, 7, 2, 9, 5]))
+
+
+# # Aufgabe 2
+# Funktion zählt, wie viele positive Zahlen in 
+# der Liste stehen
+
+def zähle_positive(liste):
+    anzahl = 0
+    for zahl in liste:
+        if zahl > 0:
+            anzahl += 1
+    return anzahl
+
+# Test / Kontrolle
+print(zähle_positive([1, -2, 3, 0, 5, -7]))
