c28cc003ee2a77d8ea9b91b021e67782ec433beb
emmaraupe <257969851+emmaraupe@users.noreply.github.com>
2026-02-16 07:49:16 +0100

    aufgaben geloest

---
+++ Hausaufgaben/202060213_hausaufgaben_listen.py
@@ -0,0 +1,40 @@
+# Aufgabe 1
+# Gegeben ist folgende Liste:
+# liste = [3, 26, -3, 0.5, "apfel"]
+# Man kann sich mit print(liste) eine komplette Liste auf der Kommandozeile ausgeben lassen.
+# Gib das allererste und das letzte Element der Liste auf die Kommandozeile aus. 
+# Beim Zugriff auf das letzte Element der Liste gibt es zwei verschiedene Möglichkeiten, zeige beide.
+liste = [3, 26, -3, 0.5, "apfel"]
+print(liste[0], liste[4])  # Zugriff auf das erste und das letzte Element der Liste mit positivem Index
+print(liste[-1]) # Zugriff auf das letzte Element der Liste mit negativem Index
+
+
+
+# Aufgabe 2
+# Was ist die Ausgabe von print(liste[-3])? Überlege es dir vorher und probiere es dann aus.
+print(liste[-3]) 
+
+
+# Aufgabe 3
+# Modifiziere die Liste so dass aus der 26 eine 27 wird.
+# Die Liste sollte dann so aussehen: [3, 27, -3, 0.5, "apfel"]
+liste[1] = 27 
+
+
+
+# Aufgabe 4
+# Füge zwischen der -3 und der 0.5 den String "birne" ein. Nutze dafür eine der Funktionen, die Listen bereits mitbringen (liste.append() etc.)
+# Die Liste sollte dann so aussehen: [3, 27, -3, "birne", 0.5, "apfel"]
+liste.insert(3, "birne")
+print(liste)
+
+
+# Aufgabe 5
+# Gib alle Elemente mit ungeradem Index (1, 3, ...) auf die Kommandozeile aus.
+# Die Ausgabe sollte dann so aussehen:
+# 27
+# birne
+# apfel
+for listenwert in range(len(liste)):
+    if listenwert % 2 == 1: 
+        print(liste[listenwert])  
