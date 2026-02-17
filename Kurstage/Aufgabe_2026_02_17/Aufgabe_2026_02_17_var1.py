1044c9422f8d6edaceadfc117b63d409439b920a
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-17 13:48:20 +0100

    Variante 1 fertigErgebnis: -92

---
+++ Kurstage/Aufgabe_2026_02_17/Aufgabe_2026_02_17_var1.py
@@ -0,0 +1,21 @@
+liste = [3, 9, 6, 6, 5, 5, 5, 4, 5, 8, 2, 8, 6, 7, 3, 7, 6, 8, 6, 9, 9, 3, 7, 9, 1, 5, 5, 7, 8, 5, 2, 1, 3, 9, 7, 9, 8, 9, 6, 9, 6, 5, 6, 3, 3, 7, 4, 9, 9, 5, 4, 9, 9, 8, 1, 9, 8, 9, 8, 6, 1, 8, 3, 3, 2, 4, 9, 2, 6, 2, 9, 3, 9, 6, 8, 4, 5, 4, 4, 3, 8, 8, 1, 3, 4, 2, 8, 3, 5, 7, 4, 4, 1, 3, 3, 3, 2, 9, 9, 8]
+
+code = 0
+
+# Alternative 1
+for index in range(len(liste)):
+    wert = liste[index]
+
+    # Wert gerade: addieren
+    if wert % 2 == 0:
+        if index % 2 == 0:
+            code = code + 2 * wert
+        else:
+            code = code + wert
+    # Wert ungerade: subtrahieren
+    else:
+        if index % 2 == 0:
+            code = code - 2 * wert
+        else:
+            code = code - wert
+print(code)
