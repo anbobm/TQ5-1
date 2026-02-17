66b758f5b42def6fa783f730aa38579bf44b285e
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-17 15:37:00 +0100

    Aufgabe 3_21 fertig

--- .vscode/Hausaufgabe_2026_02_17/Hausaufgabe_2026_02_17_1_6.py
+++ .vscode/Hausaufgabe_2026_02_17/Hausaufgabe_2026_02_17_1_6.py
@@ -23,3 +23,17 @@ print(student["Note"])
 # Test
 # print(student)
 print(student["Note"])
+
+
+# Aufgabe 3 
+# Aufgabe 3 (DE — условие задания)
+
+student = {
+    "Name": "Alice",
+    "Alter": 20,
+    "Note": "befriedigend"
+}
+
+student["Alter"] = 21
+
+print(student)
