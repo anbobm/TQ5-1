b2e0ad417902f9298c6b3eba9fa546be2b51747f
ihornosach <257969028+ihornosach@users.noreply.github.com>
2026-02-18 15:11:19 +0100

    lösung zugefügt

--- Hausaufgaben/20260218_Aufgabe/classarbeit.py
+++ Hausaufgaben/20260218_Aufgabe/classarbeit.py
@@ -1,35 +1,37 @@
-handy = {
-    "marke": "Samsung",
-    "modell": "A50",
-    "speicher": "1 tb",
-    "farbe": "Dark",
-    "betriebssystem": "Android",
-    "preis": 120 
-}
+# handy = {
+#     "marke": "Samsung",
+#     "modell": "A50",
+#     "speicher": "1 tb",
+#     "farbe": "Dark",
+#     "betriebssystem": "Android",
+#     "preis": 120 
+# }
 
-key = ""
-while key != "exit":
+# key = ""
+# while key != "exit":
 
-    key = input("Geben Sie ein Schluessel ein: ").lower()
+#     key = input("Geben Sie ein Schluessel ein: ").lower()
 
-    if key in handy:
-        print(handy[key])
-    else:
-        print("Der Schlüssel existiert nicht")
+#     if key in handy:
+#         print(handy[key])
+#     else:
+#         print("Der Schlüssel existiert nicht")
 
-#Aufgabe 2 
+# #Aufgabe 2 
 
-students = {
-    "Max": {
-        "Alter": 22, 
-        "Note":"good", 
-        "Studiengang": "Biologie"
-        },
-    "Petra": {
-        "Alter": 27, 
-        "Note":"befriedigend", 
-        "Studiengang": "Mathe"
-    }
-}
+# students = {
+#     "Max": {
+#         "Alter": 22, 
+#         "Note":"good", 
+#         "Studiengang": "Biologie"
+#         },
+#     "Petra": {
+#         "Alter": 27, 
+#         "Note":"befriedigend", 
+#         "Studiengang": "Mathe"
+#     }
+# }
 
-print("Die Note von Petra ist: " + students["Petra"]["Note"])
+# print("Die Note von Petra ist: " + students["Petra"]["Note"])
+
+
---
+++ Hausaufgaben/20260218_Aufgabe/lösung_hg.py
@@ -0,0 +1,113 @@
+#Aufgabe 1 
+
+# wort = input("Geben Sie ein Wort ein: ")
+# print(wort)
+
+# dict = {}
+
+# for buchstaben in wort:
+#     if buchstaben in dict:
+#         dict[buchstaben] = dict[buchstaben] + 1        
+#     else:
+#         dict[buchstaben] = 1
+
+# for item in dict:
+#     print(f"{item} kommt {dict[item]} mal vor")
+
+#Aufgabe 2 
+
+
+
+def main():
+
+    lager = {
+    "Apfel": {"preis": 2, "menge": 10},
+    "Banane": {"preis": 1, "menge": 8},
+    "Orange": {"preis": 3, "menge": 5}
+    }
+
+    while True:
+        show_menue()
+        launch_app(lager)
+
+def show_menue():
+    print("1. Kompletten Lagerbestand anzeigen")
+    print("2. Lagerbestand von einem Artikel anzeigen")
+    print("3. Menge von Artikeln entfernen (Verkauf)")
+    print("4. Menge von Artikeln zufuegen (Einkauf)")
+    print("5. Artikel loeschen")
+
+def launch_app(lager): 
+    menue_auswahl = int(input("Waehlen Sie Menuepunkt aus: "))
+
+    match menue_auswahl:
+        case 1:
+            komplett_lagerbestand_anzeigen(lager)               
+        case 2: 
+            artikel_lagerbestand_anzeigen(lager) 
+        case 3:
+            menge_artikel_entfernen(lager)          
+        case 4:
+            menge_artikel_zufuegen(lager)         
+        case 5:
+            artikel_loeschen(lager)            
+        case 6:
+            exit()
+
+#####
+def komplett_lagerbestand_anzeigen(lager):
+    for key, value in lager.items():
+        print(f"Artikel {key}: Preis: {value["preis"]}, Menge: {value["menge"]}")
+
+#####
+def artikel_lagerbestand_anzeigen(lager):
+    artikel = input("Welche Artikel? ")   
+    
+    if artikel in lager:
+        __artikel = lager[artikel]
+        print(f"Preis: {__artikel["preis"]} \nMenge: {__artikel["menge"]}")
+    else:
+        print("Artikel ist nicht gefunden")
+
+#####
+def menge_artikel_entfernen(lager):
+    artikel = input("Welche Artikel? ")
+
+    count_entnehmen = int(input("Wie viele entnehmen? "))
+    if artikel in lager:
+        __artikel = lager[artikel]
+        print(f"{count_entnehmen} Stueck entnommen.")
+        rest = __artikel["menge"] - count_entnehmen
+        __artikel["menge"] = rest
+        print(f"Es sind noch {rest} uebrig.")
+    else:
+        print("Artikel ist nicht gefunden")
+
+#####
+def menge_artikel_zufuegen(lager):
+    artikel = input("Welche Artikel? ")
+
+    count_hinzufügen = int(input("Wie viele hinzufügen? "))
+    if artikel in lager:
+        __artikel = lager[artikel]     
+        __count = __artikel["menge"] + count_hinzufügen
+        __artikel["menge"] = __count
+        print(f"Es sind noch {__count} uebrig.")
+    else:
+        print("Artikel ist nicht gefunden")
+
+#####
+def artikel_loeschen(lager):
+    artikel = input("Welche Artikel? ")
+
+    if artikel in lager:    
+        __artikel = lager[artikel]     
+        __artikel["menge"] = 0
+        lager[artikel] = __artikel
+        print(f" Lagerbestand auf 0 gesetzt.")
+    else:
+        print("Artikel ist nicht gefunden")
+
+
+main()
+
