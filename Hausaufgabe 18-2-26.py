2c7b3b7d0f4cebdbd9047a0a5f4d985c3d8f2709
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-19 08:29:38 +0100

    6. Menü hinzugefügt

--- Hausaufgabe 18-2-26.py
+++ Hausaufgabe 18-2-26.py
@@ -21,41 +21,45 @@ lager = {
     "Banane": {"preis": 1, "menge": 8},
     "Orange": {"preis": 3, "menge": 5}
 }
-
-Auswahl = input("1: Kompletten Lagerbestand anzeigen\n2: Lagerbestand von einem Artikel anzeigen\n3: Menge von Artikeln entfernen (Verkauf)\n4: Menge von Artikeln zufügen (Einkauf)\n5: Artikel löschen\n")
-if Auswahl == "1":
-    for artikel in lager:
-        print("Artikel:", artikel, "Preis:", str(lager[artikel]["preis"]) + "€", "Menge:", lager[artikel]["menge"])
-elif Auswahl == "2":
-    Artikel = input("Welcher Artikel: ")
-    if Artikel in lager:
-        print("Preis:", str(lager[Artikel]["preis"]) + "€")
-        print("Menge:", lager[Artikel]["menge"])
-    else:
-        print("Der Artikel existiert nicht")
-elif Auswahl == "3":
-    Artikel = input("Welcher Artikel: ")
-    Menge = int(input("Wie viele entnehmen? "))
-    if Artikel in lager:
-        if Menge <= lager[Artikel]["menge"]:
-            lager[Artikel]["menge"] = lager[Artikel]["menge"] - Menge
-            print("Es sind noch" , lager[Artikel]["menge"], Artikel , "übrig.")
+menu = True
+while menu == True:
+    Auswahl = input("1: Kompletten Lagerbestand anzeigen\n2: Lagerbestand von einem Artikel anzeigen\n3: Menge von Artikeln entfernen (Verkauf)\n4: Menge von Artikeln zufügen (Einkauf)\n5: Artikel löschen\n6: Programm beenden\n")
+    if Auswahl == "1":
+        for artikel in lager:
+            print("Artikel:", artikel, "Preis:", str(lager[artikel]["preis"]) + "€", "Menge:", lager[artikel]["menge"])
+    elif Auswahl == "2":
+        Artikel = input("Welcher Artikel: ")
+        if Artikel in lager:
+           print("Preis:", str(lager[Artikel]["preis"]) + "€")
+           print("Menge:", lager[Artikel]["menge"])
         else:
-            print("Nicht genügend Menge vorhanden")
-    else:
+             print("Der Artikel existiert nicht")
+    elif Auswahl == "3":
+      Artikel = input("Welcher Artikel: ")
+      Menge = int(input("Wie viele entnehmen? "))
+      if Artikel in lager:
+            if Menge <= lager[Artikel]["menge"]:
+              lager[Artikel]["menge"] = lager[Artikel]["menge"] - Menge
+              print("Es sind noch" , lager[Artikel]["menge"], Artikel , "übrig.")
+            else:
+              print("Nicht genügend Menge vorhanden")
+      else:
         print("Der Artikel existiert nicht")
-elif Auswahl == "4":
-    Artikel = input("Welcher Artikel: ")
-    Menge = int(input("Wie viele hinzufügen? "))
-    if Artikel in lager:
-        lager[Artikel]["menge"] = lager[Artikel]["menge"] + Menge
-        print("Es sind jetzt", lager[Artikel]["menge"], Artikel, "vorrätig.")
-    else:
-        print("Der Artikel existiert nicht")
-elif Auswahl == "5":
-    Artikel = input("Welcher Artikel:: ")
-    if Artikel in lager:
-        lager[Artikel]["menge"] = 0
-        print("Lagerbestand von", Artikel, "auf 0 gesetzt.")
-    else:
-        print("Der Artikel existiert nicht")
+    elif Auswahl == "4":
+        Artikel = input("Welcher Artikel: ")
+        Menge = int(input("Wie viele hinzufügen? "))
+        if Artikel in lager:
+            lager[Artikel]["menge"] = lager[Artikel]["menge"] + Menge
+            print("Es sind jetzt", lager[Artikel]["menge"], Artikel, "vorrätig.")
+        else:
+            print("Der Artikel existiert nicht")
+    elif Auswahl == "5":
+        Artikel = input("Welcher Artikel: ")
+        if Artikel in lager:
+            lager[Artikel]["menge"] = 0
+            print("Lagerbestand von", Artikel, "auf 0 gesetzt.")
+        else:
+            print("Der Artikel existiert nicht")
+    elif Auswahl == "6":
+        print("Programm wird beendet.")
+        menu = False
