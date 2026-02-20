563f30e6fcb3532eede7a41227fe74ca40b30a63
Andreas <foo@bar.com>
2026-02-20 09:04:50 +0100

    Aufgabe und Lösung Bankkonto zugefügt

---
+++ Kurstage/2026-02-20/aufgabe.md
@@ -0,0 +1,5 @@
+# Aufgabe
+ 
+Erstelle eine Klasse `Bankkonto` mit den Attributen `inhaber` (String, anfangs `""`)  und `kontostand` (Ganzzahl, anfangs `0`).
+ 
+Erzeuge anschließend zwei Objekte aus dieser Klasse und gib ihren Attributen konkrete Werte.
---
+++ Kurstage/2026-02-20/lösung.py
@@ -0,0 +1,13 @@
+class Bankkonto:
+    def __init__(self):
+        self.inhaber = ""
+        self.kontostand = 0
+
+
+konto1 = Bankkonto()
+konto1.inhaber = "Max"
+konto1.kontostand = 100
+
+konto2 = Bankkonto()
+konto2.inhaber = "Petra"
+konto2.kontostand = 950
