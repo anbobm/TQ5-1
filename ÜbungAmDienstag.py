cc4e1c1116c64588a51382f1e35ad4ed9128fed9
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-02-24 14:32:47 +0100

    Übung am Dienstag

---
+++ ÜbungAmDienstag.py
@@ -0,0 +1,36 @@
+print("Das ist ein Programm, das Fahrenheit in Celsius umrechnet und andersrum.")
+
+def fahrenheit_in_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+
+def celsius_in_fahrenheit(celsius):
+    fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit
+
+Auswahl = input("Möchtest du Fahrenheit in Celsius haben (1) oder Celsius in Fahrenheit (2)? ")
+Wahrhaftig = False
+
+while Wahrhaftig == False:
+    if Auswahl == "1":
+        try:
+            fahrenheit = float(input("Gib die Temperatur in Fahrenheit ein(ohne Zeichen): "))
+        except ValueError:
+            print("Ungültige Eingabe. Bitte gib eine Zahl ein.")
+            continue
+        else:
+            celsius = fahrenheit_in_celsius(fahrenheit)
+            print(f"{fahrenheit} Grad Fahrenheit entsprechen {celsius:.2f} Grad Celsius.")
+            Wahrhaftig = True
+    elif Auswahl == "2":
+        try:
+            celsius = float(input("Gib die Temperatur in Celsius ein(ohne Zeichen): "))
+        except ValueError:
+            print("Ungültige Eingabe. Bitte gib eine Zahl ein.")
+            continue
+        else:
+            fahrenheit = celsius_in_fahrenheit(celsius)
+            print(f"{celsius} Grad Celsius entsprechen {fahrenheit:.2f} Grad Fahrenheit.")
+            Wahrhaftig = True
+    else:
+        Auswahl = input("Gib eine Auswahl zwischen 1 und 2 ein: ")
