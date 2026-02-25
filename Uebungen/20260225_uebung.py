ae60132ea791d105cbb00cb1ef18b13cdedec008
emmaraupe <257969851+emmaraupe@users.noreply.github.com>
2026-02-25 10:29:34 +0100

    korrigiert

--- Uebungen/20260225_uebung.py
+++ Uebungen/20260225_uebung.py
@@ -17,22 +17,28 @@ class Katze(Haustier):
         super().ausgabe()
         print(f"Ich bin eine Katze")
         print(f"Meine Rasse ist {self.rasse}")
+
 class Hund(Haustier):
     def __init__(self, besitzer, name, impfstatus):
         super().__init__(besitzer, name)
-        self.impfstatus = "geimpft"
+        self.impfstatus = impfstatus
+    
     def bellen(self):
         print("Wuff!")
         
-    def ausgabe(self, impfstatus): 
-        impfstatus = "geimpft" 
+    def ausgabe(self):
+        super().ausgabe()
+        print("Ich bin ein Hund und")  
         if self.impfstatus == "geimpft":
-            impfstatus = "geimpft"
+            print("geimpft")
         else:
-            impfstatus = "nicht geimpft" 
-        return f"Hund: {self.name}, Status: {impfstatus}"
+           print("nicht geimpft") 
+        return f"Hund: {self.name}, Status: {self.impfstatus}"
 
+Hund = Hund("Angela", "Porti", "geimpft")
 
+Hund.ausgabe()
+Hund.bellen()
  
 # Die Klasse Hund soll ein eigenes Attribut geimpft bekommen.
 # Überschreibe in der Klasse Hund die Ausgabemethode.
