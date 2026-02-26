8bded247ea2b887736e823111c674b98d5af0292
emmaraupe <257969851+emmaraupe@users.noreply.github.com>
2026-02-26 14:10:53 +0100

    korrigiert

--- Uebungen/20260226_uebung4_temperatur.py
+++ Uebungen/20260226_uebung4_temperatur.py
@@ -10,32 +10,48 @@
 # erhoehen(wert): Erhöht die Temperatur um den übergebenen Wert
 # senken(wert): Senkt die Temperatur um den übergebenen Wert.
 # Hinweis: Die Temperatur darf nie kleiner als -273.15 sein.
-
+import math
 class Temperatur:
     def __init__(self):
-        self.__temp_celsius = 0.0
+        self._temp_celsius = 0
         
     def set_celsius(self, wert):
-        self.__temp_celsius = wert
-        if wert >= -273.15
+        self._temp_celsius = wert
+        if wert >= -273.15:
             print(f"Temperatur auf {wert}°C gesetzt.")
         else:
             print("Die Temperatur kann nicht unter -273.15 °C liegen.")
         
     
     def get_celsius(self): 
-        return self.__temp_celsius
+        print(self._temp_celsius)
         
     def get_fahrenheit(self):
-        return self.__temp_celsius * 9/5 + 32
+        print(f"Temperatur von {self._temp_celsius}°C sind {self._temp_celsius * 9/5 + 32}° Fahrenheit")
     
     def erhoehen(self, wert):
-        neue_temp = self.__temp_celsius + wert 
-        return f"Temperatur um {wert} °C erhöht."
+        self.temp_celsius = self._temp_celsius + wert 
+        print(f"Temperatur um {wert}°C erhöht.Es sind jetzt {self._temp_celsius + wert}°C.")
     
     def senken(self, wert):
-        neue_temp = self.__temp_celsius - wert
+        self.temp_celsius = self._temp_celsius - wert
         if wert <= -273.15:
             print("Die Temperatur kann nicht unter -273.15°C liegen.")
         else:
-            return f"Temperatur um {wert} °C gesenkt."
+            print(f"Temperatur um {wert}°C gesenkt. Es sind jetzt {self._temp_celsius - wert}°C.")
+        
+temp1 = Temperatur()
+temp2 = Temperatur()
+
+temp1.set_celsius(20)
+temp2.set_celsius(33)
+
+
+
+temp1.erhoehen(7)
+print(temp1.get_celsius())
+print(temp1.get_fahrenheit())
+
+temp2.senken(17)
+print(temp2.get_fahrenheit())
+
