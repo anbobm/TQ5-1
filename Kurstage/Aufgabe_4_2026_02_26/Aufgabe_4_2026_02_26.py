afb3dc445fde9dcd67984fe2931aa73a58a80590
natahariharinata-eng <natahariharinata@gmail.com>
2026-02-26 14:35:36 +0100

    Aufgabe_4_2026_02_26 fertig

--- Kurstage/Aufgabe_4_2026_02_26/Aufgabe_4_2026_02_26.py
+++ Kurstage/Aufgabe_4_2026_02_26/Aufgabe_4_2026_02_26.py
@@ -16,8 +16,8 @@ class Temperatursensor:
         return self.__celsius * 9 / 5 + 32
     
     def erhoehen(self, wert):
-        neue_temperatur = self.__celsius + wert
-        if neue_temperatur < -273.15:
+        neue_temp = self.__celsius + wert
+        if neue_temp < -273.15:
             self.__celsius = -273.15
         
         else:
@@ -29,6 +29,26 @@ class Temperatursensor:
             self.__celsius = -273.15
         else:
             self.__celsius = neue_temp
+
+
+if __name__ == "__main__":
+    sensor = Temperatursensor()
+
+    print("Start:", sensor.get_celsius())
+
+    sensor.set_celsius(20)
+    print("Nach set_celsius(20):", sensor.get_celsius())
+
+    print("In Fahrenheit:", sensor.get_fahrenheit())
+
+    sensor.erhoehen(10)
+    print("Nach erhoehen(10):", sensor.get_celsius())
+
+    sensor.senken(50)
+    print("Nach senken(50):", sensor.get_celsius())
+
+    sensor.senken(500)
+    print("Nach senken(500) (Grenze prüfen):", sensor.get_celsius())            
 
 
     
