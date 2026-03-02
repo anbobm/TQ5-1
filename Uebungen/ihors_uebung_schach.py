c363172f4c118d2b77c4b9e2b310784ddb7e1273
emmaraupe <257969851+emmaraupe@users.noreply.github.com>
2026-03-02 12:37:22 +0100

    aufgabe erstellt

---
+++ Uebungen/ihors_uebung_schach.py
@@ -0,0 +1,7 @@
+# Wenn du möchtest)) 
+# Schreibe ein Programm, das berechnet, wie viele gültige Züge der Springer von dieser Position aus machen kann.
+# Ein Zug ist gültig, wenn:
+# • der Springer sich nach den Schachregeln bewegt (2 Felder in eine Richtung und 1 Feld senkrecht dazu),
+# • und die neue Position sich innerhalb des Schachbretts befindet (Koordinaten zwischen 1 und 8).
+
+
