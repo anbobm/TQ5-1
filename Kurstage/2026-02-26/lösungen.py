dfb8d8278e08ba4667a9616a2b25c83af93e4374
Andreas <foo@bar.com>
2026-03-03 10:26:27 +0100

    Lösungen ergänzt

--- Kurstage/2026-02-26/lösungen.py
+++ Kurstage/2026-02-26/lösungen.py
@@ -1,128 +1,128 @@
 # Aufgabe 1
 
-# class Produkt:
-#     def __init__(self, name, preis):
-#         self._name = name
-#         self._preis = preis
-#         self._lagerbestand = 0
+class Produkt:
+    def __init__(self, name, preis):
+        self._name = name
+        self._preis = preis
+        self._lagerbestand = 0
 
-#     def verkaufen(self, menge):
-#         if not menge > 0:
-#             print("Negative Anzahl kann man nicht verkaufen")
-#             return
+    def verkaufen(self, menge):
+        if not menge > 0:
+            print("Negative Anzahl kann man nicht verkaufen")
+            return
         
-#         if self._lagerbestand >= menge:
-#             self._lagerbestand = self._lagerbestand - menge
-#         else:
-#             print("Nicht genug vorrätig")
+        if self._lagerbestand >= menge:
+            self._lagerbestand = self._lagerbestand - menge
+        else:
+            print("Nicht genug vorrätig")
 
-#     def nachbestellen(self, menge):
-#         if not menge > 0:
-#             print("Negative Anzahl kann man nicht nachbestellen")
-#             return
+    def nachbestellen(self, menge):
+        if not menge > 0:
+            print("Negative Anzahl kann man nicht nachbestellen")
+            return
 
-#         self._lagerbestand = self._lagerbestand + menge
+        self._lagerbestand = self._lagerbestand + menge
 
-#     def set_preis(self, neuer_preis):
-#         if neuer_preis < 0:
-#             print("Negative Preise sind nicht erlaubt")
-#             return
+    def set_preis(self, neuer_preis):
+        if neuer_preis < 0:
+            print("Negative Preise sind nicht erlaubt")
+            return
         
-#         self._preis = neuer_preis
+        self._preis = neuer_preis
 
-#     def get_info(self):
-#         print(f"Das Produkt heißt {self._name} und es kostet {self._preis}.")
-#         print(f"Zur Zeit sind im Lager {self._lagerbestand} vorrätig.")
+    def get_info(self):
+        print(f"Das Produkt heißt {self._name} und es kostet {self._preis}.")
+        print(f"Zur Zeit sind im Lager {self._lagerbestand} vorrätig.")
 
-# produkt = Produkt("Apfel", 1)
+produkt = Produkt("Apfel", 1)
 
-# produkt.get_info()
-# produkt.verkaufen(1)
-# produkt.get_info()
-# produkt.nachbestellen(10)
-# produkt.get_info()
-# produkt.verkaufen(7)
-# produkt.get_info()
-# produkt.verkaufen(3)
-# produkt.get_info()
-# produkt.set_preis(2)
-# produkt.get_info()
+produkt.get_info()
+produkt.verkaufen(1)
+produkt.get_info()
+produkt.nachbestellen(10)
+produkt.get_info()
+produkt.verkaufen(7)
+produkt.get_info()
+produkt.verkaufen(3)
+produkt.get_info()
+produkt.set_preis(2)
+produkt.get_info()
 
-# produkt.verkaufen(-3)
-# produkt.nachbestellen(-10)
-# produkt.set_preis(-1)
+produkt.verkaufen(-3)
+produkt.nachbestellen(-10)
+produkt.set_preis(-1)
 
 # Aufgabe 2
 
-# class Rechteck:
-#     def __init__(self, hoehe, breite):
-#         self._hoehe = hoehe
-#         self._breite = breite
+class Rechteck:
+    def __init__(self, hoehe, breite):
+        self._hoehe = hoehe
+        self._breite = breite
 
-#     def set_breite(self, wert):
-#         if wert <= 0:
-#             print("Nur positive Werte als Breite erlaubt")
-#             return
+    def set_breite(self, wert):
+        if wert <= 0:
+            print("Nur positive Werte als Breite erlaubt")
+            return
         
-#         self._breite = wert
+        self._breite = wert
 
-#     def set_hoehe(self, wert):
-#         if wert <= 0:
-#             print("Nur positive Werte als Höhe erlaubt")
-#             return
+    def set_hoehe(self, wert):
+        if wert <= 0:
+            print("Nur positive Werte als Höhe erlaubt")
+            return
         
-#         self._hoehe = wert
+        self._hoehe = wert
 
-#     def flaeche(self):
-#         return self._breite * self._hoehe
+    def flaeche(self):
+        return self._breite * self._hoehe
     
-#     def umfang(self):
-#         return 2 * (self._hoehe + self._breite)
+    def umfang(self):
+        return 2 * (self._hoehe + self._breite)
 
 
-# rechteck = Rechteck(10, 20)
+rechteck = Rechteck(10, 20)
 
-# print(rechteck.flaeche())
-# print(rechteck.umfang())
+print(rechteck.flaeche())
+print(rechteck.umfang())
 
-# rechteck.set_breite(-1)
-# rechteck.set_hoehe(-1)
+rechteck.set_breite(-1)
+rechteck.set_hoehe(-1)
 
 # Aufgabe 3
 
-# class Benutzer:
-#     def __init__(self, benutzername, passwort):
-#         self._benutzername = benutzername
-#         self._passwort = passwort
-#         self._ist_eingeloggt = False
+class Benutzer:
+    def __init__(self, benutzername, passwort):
+        self._benutzername = benutzername
+        self._passwort = passwort
+        self._ist_eingeloggt = False
 
-#     def login(self, passwort):
-#         if passwort != self._passwort:
-#             print("Passwort ist falsch")
-#             return
+    def login(self, passwort):
+        if passwort != self._passwort:
+            print("Passwort ist falsch")
+            return
         
-#         self._ist_eingeloggt = True
+        self._ist_eingeloggt = True
 
-#     def logout(self):
-#         self._ist_eingeloggt = False
+    def logout(self):
+        self._ist_eingeloggt = False
 
-#     def passwort_ändern(self, altes_pw, neues_pw):
-#         if altes_pw != self._passwort:
-#             print("Falsches Passwort")
-#             return
+    def passwort_ändern(self, altes_pw, neues_pw):
+        if altes_pw != self._passwort:
+            print("Falsches Passwort")
+            return
         
-#         if len(neues_pw) < 8:
-#             print("Neues Passwort muss mindestens 8 Zeichen lang sein")
-#             return
+        if len(neues_pw) < 8:
+            print("Neues Passwort muss mindestens 8 Zeichen lang sein")
+            return
 
-#         self._passwort = neues_pw
+        self._passwort = neues_pw
 
-#     def eingeloggt(self):
-#         return self._ist_eingeloggt
+    def eingeloggt(self):
+        return self._ist_eingeloggt
 
-# benutzer = Benutzer("foo", "passwort")
+benutzer = Benutzer("foo", "passwort")
 
-# benutzer.passwort_ändern("passwort", "Passwort1!")
+benutzer.passwort_ändern("passwort", "Passwort1!")
 
 # Aufgabe 4
 
@@ -185,10 +185,53 @@ print(sensor.get_celsius())
 sensor.erhoehen(400)
 print(sensor.get_celsius())
 
-# print(sensor.get_celsius())
-# sensor.set_celsius(30)
-# print(sensor.get_celsius())
-# print(sensor.get_fahrenheit())
+print(sensor.get_celsius())
+sensor.set_celsius(30)
+print(sensor.get_celsius())
+print(sensor.get_fahrenheit())
 
-# sensor.set_celsius(-273.151)
+sensor.set_celsius(-273.151)
     
+
+
+# Aufgabe 5
+
+class Mitarbeiter:
+    def __init__(self, name, gehalt):
+        self._name = name
+        self._gehalt = gehalt
+
+    def get_gehalt(self):
+        return self._gehalt
+    
+    def gehalt_erhoehen(self, prozent):
+        if prozent <= 0:
+            print("Nur positive Werte zugelassen")
+            return
+        
+        self._gehalt = self._gehalt * (1 + prozent / 100)
+
+
+class Manager(Mitarbeiter):
+    def __init__(self, name, gehalt, bonus):
+        super().__init__(name, gehalt)
+        self._bonus = bonus
+
+    def get_gehalt(self):
+        return super().get_gehalt() + self._bonus
+    
+    def set_bonus(self, bonus):
+        if self._bonus < 0:
+            print("Es gibt keine negativen Boni")
+            return
+        
+        self._bonus = bonus
+
+mitarbeiter = Mitarbeiter("Max", 1000)
+
+mitarbeiter.gehalt_erhoehen(10)
+print(mitarbeiter.get_gehalt())
+
+manager = Manager("Max", 4000, 1000)
+
+print(manager.get_gehalt())
