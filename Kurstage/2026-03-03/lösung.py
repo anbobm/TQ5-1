059eb02a38c6beeacaedb536c1748f284b914b1b
Andreas <foo@bar.com>
2026-03-05 10:43:37 +0100

    Lösung zugefügt

--- Kurstage/2026-03-03/lösung.py
+++ Kurstage/2026-03-03/lösung.py
@@ -1,20 +1,53 @@
+from random import randint
+
 class Charakter:
     def __init__(self, name, leben):
         self._name = name
         self._leben = leben
         self._schaden = 10
+        self.effekte : list[StatusEffekt] = []
     
     def angreifen(self, ziel):
-        print(f"{self._name} hat {self._schaden} Schaden verteilt.")
-        ziel.erleiden_schaden(self._schaden)
+        schaden = self._schaden - randint(0, int(self._schaden * 0.5))
+
+        # Effekte können schaden beeinflussen
+        for effekt in self.effekte:
+            schaden = effekt.beim_angreifen(schaden)
+
+        print(f"{self._name} hat {schaden} Schaden verteilt.")
+        ziel.erleiden_schaden(schaden)
     
     def erleiden_schaden(self, schaden):
+        # Effekte können erlittenen Schaden beeinflussen
+        for effekt in self.effekte:
+            schaden = effekt.beim_schaden_erleiden(schaden)
+
         self._leben -= schaden
 
         if self._leben < 0:
             self._leben = 0
 
-        print(f"{self._name} hat {schaden} Schaden erlitten und jetzt noch {self._leben} Leben übrig")
+        if self._leben == 0:
+            print(f"{self._name} hat {schaden} Schaden erlitten und ist gestorben")
+        else:
+            print(f"{self._name} hat {schaden} Schaden erlitten und jetzt noch {self._leben} Leben übrig")
+
+    def ist_tot(self):
+        return self._leben <= 0
+    
+    def effekt_hinzufuegen(self, effekt):
+        self.effekte.append(effekt)
+
+    def neue_runde(self):
+        verbleibende_effekte = []
+
+        for effekt in self.effekte:
+            effekt.beim_rundenstart(self)
+            effekt.runde_verringern()
+            if not effekt.ist_abgelaufen():
+                verbleibende_effekte.append(effekt)
+        
+        self.effekte = verbleibende_effekte
 
 class Magier(Charakter):
     def __init__(self, name, leben, mana):
@@ -51,8 +84,53 @@ class Krieger(Charakter):
 
         super().erleiden_schaden(reduzierter_schaden)
 
+class StatusEffekt:
+    def __init__(self, dauer):
+        self.dauer = dauer
+    
+    def beim_rundenstart(self, charakter):
+        pass
+
+    def beim_angreifen(self, schaden):
+        return schaden
+    
+    def beim_schaden_erleiden(self, schaden):
+        return schaden
+    
+    def runde_verringern(self):
+        self.dauer -= 1
+
+    def ist_abgelaufen(self):
+        return self.dauer <= 0
+    
+class Schwäche(StatusEffekt):
+    def beim_angreifen(self, schaden):
+        return int(schaden * 0.9)
+    
+class Vergiftet(StatusEffekt):
+    def beim_rundenstart(self, charakter):
+        charakter._leben -= 3
+        print(f"{charakter._name} ist vergiftet und verliert 3 Lebenspunkte und hat jetzt noch {charakter._leben} Lebenspunkte")
 
 magier = Magier("Gandalf", 100, 100)
-krieger = Krieger("Aragorn", 100, 20)
+krieger = Krieger("Aragorn", 100, 0)
 
-magier.zaubern(krieger)
+krieger.effekt_hinzufuegen(Vergiftet(5))
+
+runde = 1
+while not magier.ist_tot() and not krieger.ist_tot():
+    print(f"Runde: {runde}")
+
+    magier.neue_runde()
+    krieger.neue_runde()
+
+    # kämpfen
+    magier.angreifen(krieger)
+
+    if krieger.ist_tot():
+        break
+
+    krieger.angreifen(magier)
+
+    runde += 1
+
