1e905e6209c7a8d39cbe8227b55e24bd7e2b2d5e
Andreas <foo@bar.com>
2026-03-16 08:44:50 +0100

    split() Beispiel zugefügt

---
+++ Kurstage/2026-03-16/split.py
@@ -0,0 +1,6 @@
+# str.split() Methode
+# eine String anhand eines Separators (default: alle Whitespace-Zeichen) trennen und 
+# in einer Liste zurückgeben
+
+"foo bar trallala\ndings bums bla\n\nfoo\nfoo".split()
+# ['foo', 'bar', 'trallala', 'dings', 'bums', 'bla', 'foo', 'foo']
