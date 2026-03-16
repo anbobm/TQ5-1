36f741bc2c314073f5c12378880e57e5a7462888
emmaraupe <257969851+emmaraupe@users.noreply.github.com>
2026-03-16 10:05:49 +0100

    aufgabe 2 geloest

--- Uebungen/20260316_uebung_split.py
+++ Uebungen/20260316_uebung_split.py
@@ -1,11 +1,42 @@
-# Schreibe ein Python-Skript, welches einen Satz vom Benutzer einliest (z.B. "The quick brown fox jumps over the lazy dog") und dann zählt, wie viele Wörter in diesem String sind.
+# Aufgabe 1
+# # Schreibe ein Python-Skript, welches einen Satz vom Benutzer einliest (z.B. "The quick brown fox jumps over the lazy dog") und dann zählt, wie viele Wörter in diesem String sind.
 
-zeile = "The quick brown fox jumps over the lazy dog".split()
-count = 0
-for word in zeile:
-    if word != "":
-        count = count + 1
-        print(f"Es gibt {count} Wörter im Satz.")
-        
-zeile = "The quick brown fox jumps over the lazy dog".split()
-print(f"Es gibt {len(zeile)} Wörter im Satz.")
+# zeile = "The quick brown fox jumps over the lazy dog".split()
+# count = 0
+# for word in zeile:
+#     if word != "":
+#         count = count + 1
+#         print(f"Es gibt {count} Wörter im Satz.")
+
+# # mit lenght        
+# zeile = "The quick brown fox jumps over the lazy dog".split()
+# print(f"Es gibt {len(zeile)} Wörter im Satz.")
+
+# # mit Eingabe des Nutzers
+# eingabe = input("Gib einen Satz ein: ")
+# wortliste = eingabe.split()
+# wörter = len(wortliste)
+# print(f"Es gibt {wörter} Wörter im Satz.")
+
+
+# Aufgabe 2
+# Es gibt eine Datei foobar.txt mit folgendem Inhalt. Zähle die Wörter in dieser Datei mit einem Python-Skript.
+
+datei = open("20260316_foobar.txt")
+
+inhalt = datei.readline()
+
+
+# wörter = inhalt.split()
+# print(f"Es gibt {len(wörter)} Wörter in der Datei.")
+
+anzahl = 0
+for inhalt in datei:
+    anzahl = anzahl + len(inhalt.split())
+print(f"Es gibt {anzahl} Wörter in der Datei.")   
+   
+
+
+ 
+ 
+    
