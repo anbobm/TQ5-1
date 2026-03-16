9d8cb7dcb7fb8821142a02926607ba070c23acf2
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-03-16 21:36:14 +0100

    Übung

---
+++ Übungsaufgabe 16.03.26.py
@@ -0,0 +1,32 @@
+#Schreibe ein Python-Skript, welches einen Satz vom Benutzer einliest (z.B. "The quick brown fox jumps over the lazy dog") und dann zählt, wie viele Wörter in diesem String sind.
+
+satz = input("Bitte geben Sie einen Satz ein: ")
+
+woerter = satz.split()
+anzahl_woerter = len(woerter)
+print("Die Anzahl der Wörter in Ihrem Satz ist:", anzahl_woerter)
+
+#Es gibt eine Datei foobar.txt mit folgendem Inhalt. Zähle die Wörter in dieser Datei mit einem Python-Skript.
+
+# dateiname = 'foobar.txt'
+# with open(dateiname, 'r') as datei:
+#     inhalt = datei.read()
+#     woerter_in_datei = inhalt.split()
+#     anzahl_woerter_in_datei = len(woerter_in_datei)
+#     print("Die Anzahl der Wörter in der Datei ist:", anzahl_woerter_in_datei)
+
+datei = open(r'C:\Users\Sebas\Desktop\TQ5\Repo\TQ5-1\foobar2.txt', 'r')
+inhalt = datei.read()
+woerter_in_datei = inhalt.split()
+anzahl_woerter_in_datei = len(woerter_in_datei)
+print("Die Anzahl der Wörter in der Datei ist:", anzahl_woerter_in_datei)
+datei.close()
+
+with open(r'C:\Users\Sebas\Desktop\TQ5\Repo\TQ5-1\foobar2.txt', 'r') as datei:
+    inhalt = datei.read()
+    woerter_in_datei = inhalt.split()
+    anzahl_woerter_in_datei = len(woerter_in_datei)
+    anzahl_woerter_in_datei = anzahl_woerter_in_datei // 2
+    print("Die Anzahl der Wörter in der Datei ist:", anzahl_woerter_in_datei)
+
+
