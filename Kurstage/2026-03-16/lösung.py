e4fd1700195bf6863aee4856915feba81cc59c91
Andreas <foo@bar.com>
2026-03-16 09:23:12 +0100

    Lösung ergänzt

--- Kurstage/2026-03-16/lösung.py
+++ Kurstage/2026-03-16/lösung.py
@@ -1,14 +1,25 @@
-datei = open("Kurstage/2026-03-16/foo.txt")
+# # Hausaufgabe
 
-count = 1
+# datei = open("Kurstage/2026-03-16/foo.txt")
 
-while True:
-    zeile = datei.readline()
-    if zeile == "":
-        break
-    if zeile == "bar\n":
-        print(f"bar gefunden in Zeile {count}")
-        break
-    count += 1
+# count = 1
 
-datei.close()
+# while True:
+#     zeile = datei.readline()
+#     if zeile == "":
+#         break
+#     if zeile == "bar\n":
+#         print(f"bar gefunden in Zeile {count}")
+#         break
+#     count += 1
+
+# datei.close()
+
+
+# Aufgabe 1
+
+eingabe = input("Gib einen Satz ein: ")
+wortliste = eingabe.split()
+wörter = len(wortliste)
+
+print(f"Der Satz hat {wörter} Wörter.")
