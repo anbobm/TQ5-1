253373c38a527f2f6ddc5ca0fb47c075065d7256
emmaraupe <257969851+emmaraupe@users.noreply.github.com>
2026-03-16 10:39:38 +0100

    aufgabe 3 geloest

--- Uebungen/20260316_uebung_split.py
+++ Uebungen/20260316_uebung_split.py
@@ -22,21 +22,28 @@
 # Aufgabe 2
 # Es gibt eine Datei foobar.txt mit folgendem Inhalt. Zähle die Wörter in dieser Datei mit einem Python-Skript.
 
-datei = open("20260316_foobar.txt")
+# datei = open("20260316_foobar.txt")
 
-inhalt = datei.readline()
+# inhalt = datei.readline()
 
 
 # wörter = inhalt.split()
 # print(f"Es gibt {len(wörter)} Wörter in der Datei.")
 
-anzahl = 0
-for inhalt in datei:
-    anzahl = anzahl + len(inhalt.split())
-print(f"Es gibt {anzahl} Wörter in der Datei.")   
+# anzahl = 0
+# for inhalt in datei:
+#     anzahl = anzahl + len(inhalt.split())
+# print(f"Es gibt {anzahl} Wörter in der Datei.")   
    
+# Aufgabe 3
+# Es gibt eine Datei foobar2.txt mit folgendem Inhalt. Zähle wie oft das Wort foo in dieser Datei vorkommt.
+datei = open("20260316_foobar2.txt")
 
+anzahl = 0
 
- 
+for word in datei.read().split():
+    if word == "foo":
+        anzahl = anzahl + 1
+print(f"Es gibt {anzahl} mal das Wort \"foo\" in der Datei.")
  
     
