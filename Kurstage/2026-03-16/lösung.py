d9910a8b14e36d8ca4fa67fbd2777a9141a6249e
Andreas <foo@bar.com>
2026-03-16 10:48:53 +0100

    Lösung hinzugefügt

--- Kurstage/2026-03-16/lösung.py
+++ Kurstage/2026-03-16/lösung.py
@@ -15,11 +15,56 @@
 
 # datei.close()
 
+# # Aufgabe 1
 
-# Aufgabe 1
+# eingabe = input("Gib einen Satz ein: ")
+# wortliste = eingabe.split()
+# wörter = len(wortliste)
 
-eingabe = input("Gib einen Satz ein: ")
-wortliste = eingabe.split()
-wörter = len(wortliste)
+# print(f"Der Satz hat {wörter} Wörter.")
 
-print(f"Der Satz hat {wörter} Wörter.")
+
+# # Aufgabe 2
+
+# datei = open("Kurstage/2026-03-16/foobar.txt")
+
+# anzahl = 0
+# for zeile in datei:
+#     anzahl += len(zeile.split())
+# print(f"Es gibt {anzahl} Wörter in der Datei")
+
+# # Alternative 1
+# # print(f"Es gibt {len(datei.read().split())} Wörter in der Datei")
+
+# # Alternative 2
+# # anzahl = 0
+# # while True:
+# #     zeile = datei.readline()
+# #     if zeile == "":
+# #         break
+# #     anzahl += len(zeile.split())
+# # print(f"Es gibt {anzahl} Wörter in der Datei")
+
+
+# Aufgabe 3
+
+datei = open("Kurstage/2026-03-16/foobar2.txt")
+
+anzahl = 0
+for zeile in datei:
+    anzahl += zeile.split().count("foo")
+print(f"Das Wort foo kommt {anzahl} mal in der Datei vor.")
+
+# # Alternative 1
+# print(f"Es gibt {datei.read().split().count("foo")} Wörter in der Datei")
+
+# # Alternative 2
+# anzahl = 0
+# for zeile in datei:
+#     for wort in zeile.split():
+#         if wort == "foo":
+#             anzahl += 1
+# print(f"Das Wort foo kommt {anzahl} mal in der Datei vor.")
+
+
+
