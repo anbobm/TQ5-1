831490a5e9b987d89e843c76991132fbe246bd0e
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-03-17 18:37:39 +0100

    Hausaufgabe 17.3.26

--- Übungsaufgabe 17.3.26.py
+++ Übungsaufgabe 17.3.26.py
@@ -1,4 +1,4 @@
-# Aufgabe 1
+# # Aufgabe 1
 eingabe = open('C:\\Users\\Sebas\\Desktop\\TQ5\\Repo\\TQ5-1\\input.txt', 'r')
 inhalt = eingabe.read()
 eingabe.close()
@@ -11,5 +11,36 @@ for wort in wörter:
         ausgabe.write(wort + '\n')
 ausgabe.close()
 
+#Aufgabe 2
+Eingabe = input('Geben Sie einen Satz ein: ')
+Ausgabe = print(Eingabe.rstrip("bar"))
 
+
+# Aufgabe 3
+with open('dates.txt', 'r') as eingabe:
+    inhalt = eingabe.read()
+
+zeilen = inhalt.split()
+
+with open('dates_output.txt', 'w') as ausgabe:
+    for zeile in range(0, len(zeilen), 2):
+        ausgabe.write(f"Datum: {zeilen[zeile]}, \nUhrzeit: {zeilen[zeile + 1].rstrip('Uhr')}\n")
+
+#Aufgabe 4
+with open('dates.txt', 'r') as eingabe:
+    inhalt = eingabe.read()
+
+zeilen = inhalt.split()
+date = []
+for i in range(0, len(zeilen), 2):
+    date.append(zeilen[i])
+
+anzahl = 0
+with open('dates_output2.txt', 'w') as ausgabe:
+    for zeile in range(0, len(zeilen), 2):
+        d = zeilen[zeile].replace('-', '.')
+        d = d.split(".")
+        date = ".".join(reversed(d))
+        ausgabe.write(f"Datum: {date}, \nUhrzeit: {zeilen[zeile + 1].rstrip('Uhr')}\n")
+        
 
