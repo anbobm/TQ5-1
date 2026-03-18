06bdb4e81a79c048800305bd759d7f00aa9b2395
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-03-18 10:26:12 +0100

    Aufgabenupdate

--- Übungsaufgabe 17.3.26.py
+++ Übungsaufgabe 17.3.26.py
@@ -1,45 +1,56 @@
-# # Aufgabe 1
-eingabe = open('C:\\Users\\Sebas\\Desktop\\TQ5\\Repo\\TQ5-1\\input.txt', 'r')
-inhalt = eingabe.read()
-eingabe.close()
+# # # Aufgabe 1
+# eingabe = open('C:\\Users\\Sebas\\Desktop\\TQ5\\Repo\\TQ5-1\\input.txt', 'r')
+# inhalt = eingabe.read()
+# eingabe.close()
 
-ausgabe = open('C:\\Users\\Sebas\\Desktop\\TQ5\\Repo\\TQ5-1\\lange_wörter.txt', 'w')
-wörter = inhalt.split()
+# ausgabe = open('C:\\Users\\Sebas\\Desktop\\TQ5\\Repo\\TQ5-1\\lange_wörter.txt', 'w')
+# wörter = inhalt.split()
 
-for wort in wörter:
-    if len(wort) > 5:
-        ausgabe.write(wort + '\n')
-ausgabe.close()
+# for wort in wörter:
+#     if len(wort) > 5:
+#         ausgabe.write(wort + '\n')
+# ausgabe.close()
 
-#Aufgabe 2
-Eingabe = input('Geben Sie einen Satz ein: ')
-Ausgabe = print(Eingabe.rstrip("bar"))
+# #Aufgabe 2
+# Eingabe = input('Geben Sie einen Satz ein: ')
+# Ausgabe = print(Eingabe.rstrip("bar"))
 
 
-# Aufgabe 3
-with open('dates.txt', 'r') as eingabe:
-    inhalt = eingabe.read()
+# # Aufgabe 3
+# with open('dates.txt', 'r') as eingabe:
+#     inhalt = eingabe.read()
 
-zeilen = inhalt.split()
+# zeilen = inhalt.split()
 
-with open('dates_output.txt', 'w') as ausgabe:
-    for zeile in range(0, len(zeilen), 2):
-        ausgabe.write(f"Datum: {zeilen[zeile]}, \nUhrzeit: {zeilen[zeile + 1].rstrip('Uhr')}\n\n")
+# with open('dates_output.txt', 'w') as ausgabe:
+#     for zeile in range(0, len(zeilen), 2):
+#         ausgabe.write(f"Datum: {zeilen[zeile]}, \nUhrzeit: {zeilen[zeile + 1].rstrip('Uhr')}\n\n")
 
-#Aufgabe 4
-with open('dates.txt', 'r') as eingabe:
-    inhalt = eingabe.read()
+# #Aufgabe 4
+# with open('dates.txt', 'r') as eingabe:
+#     inhalt = eingabe.read()
 
-zeilen = inhalt.split()
-date = []
-for i in range(0, len(zeilen), 2):
-    date.append(zeilen[i])
+# zeilen = inhalt.split()
+# date = []
+# for i in range(0, len(zeilen), 2):
+#     date.append(zeilen[i])
 
-with open('dates_output2.txt', 'w') as ausgabe:
-    for zeile in range(0, len(zeilen), 2):
-        d = zeilen[zeile].replace('-', '.')
-        d = d.split(".")
-        date = ".".join(reversed(d))
-        ausgabe.write(f"Datum: {date}, \nUhrzeit: {zeilen[zeile + 1].rstrip('Uhr')}\n\n")
+# with open('dates_output2.txt', 'w') as ausgabe:
+#     for zeile in range(0, len(zeilen), 2):
+#         d = zeilen[zeile].replace('-', '.')
+#         d = d.split(".")
+#         date = ".".join(reversed(d))
+#         ausgabe.write(f"Datum: {date}, \nUhrzeit: {zeilen[zeile + 1].rstrip('Uhr')}\n\n")
         
+#Aufgabe 5
+datei = open("temps.txt", "r")
+zeilen = datei.read().splitlines()
+datei.close()
 
+neue_datei = open("formatted_dates.txt", "w")
+for zeile in zeilen:
+    zeile = zeile.removesuffix("Uhr")
+    temperatur, datum, uhrzeit = zeile.split()
+    jahr, monat, tag = datum.split("-")
+    neue_datei.write(f"Temperatur: {temperatur} Grad Celsius\nDatum: {tag}.{monat}.{jahr}\nUhrzeit: {uhrzeit}\n\n")
+neue_datei.close()
