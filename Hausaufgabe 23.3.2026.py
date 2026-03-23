84a4db1c3c64bf31e451ec87fd2c0b45e50c5b03
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-03-23 10:33:07 +0100

    Fertig

--- Hausaufgabe 23.3.2026.py
+++ Hausaufgabe 23.3.2026.py
@@ -1,6 +1,17 @@
 import os
 
-pfad = "C:\Users\Sebas\Desktop\TQ5\Repo\TQ5-1\Kurstage\2026-03-23\photos"
+pfad = r"C:\Users\Sebas\Desktop\TQ5\Repo\TQ5-1\photos"
 Dateien = os.listdir(pfad)
-for eintrag in Dateien:
-    print(eintrag)
+
+for datei in Dateien:
+    aufgeteilt = datei.split("-")
+    beschreibung = aufgeteilt[2]
+    datum_und_uhrzeit = aufgeteilt[3]
+    datum, zeit = datum_und_uhrzeit.split("_")
+    tag, monat, jahr = datum.split(".")
+    zeit = zeit.replace("h", "").replace("jpg", "")
+    neuer_name = f"{jahr} {monat} {tag} {zeit} {beschreibung}.jpg"
+    alt = os.path.join(pfad, datei)
+    neu = os.path.join(pfad, neuer_name)
+    os.rename(alt, neu)
+    
